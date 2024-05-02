70a7438c8a321f803819b267f18c5be9cac45d15
josephorokpo <josephorokpo@gmail.com>
2024-05-02 10:45:49 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Returns the floor of a float.
+
+    Args:
+        n (float): The float number.
+
+    Returns:
+        float: The floor of n.
+    """
+    return math.floor(n)
