2962b9a4b7d8936bdbde0c01220105d5ebd783c3
josephorokpo <josephorokpo@gmail.com>
2024-05-02 11:02:01 +0000

    4-define_variables.py

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Module: variables
+Description: Defines and annotates variables with specified values.
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
