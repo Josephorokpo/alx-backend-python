4b9b3ebf03197921d7a7d425a4225075e469b56e
josephorokpo <josephorokpo@gmail.com>
2024-05-06 13:00:21 +0000

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 This module contains an asynchronous coroutine that waits for a random delay
 between 0 and max_delay seconds, and another coroutine to spawn multiple
@@ -9,7 +8,9 @@ instances of wait_random.
 
 import asyncio
 from typing import List
-from random_delay import wait_random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
@@ -24,9 +25,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: List of delays in ascending order.
     """
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    delays.sort()  # Sort the delays in ascending order
-    return delays
-
-if __name__ == "__main__":
-    asyncio.run(wait_n(5, 10))
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
