2ded094124a0ff85aebbc81b935e2e844dd27e97
josephorokpo <josephorokpo@gmail.com>
2024-05-24 13:31:31 +0000

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,49 +1,39 @@
 #!/usr/bin/env python3
 """
-This module contains the unit tests for the `access_nested_map` function from
-the `utils` module.
+This module contains the unit tests for the `get_json` function
+from the `utils` module.
 """
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from typing import Any, Dict, Mapping, Sequence, Tuple
-from utils import access_nested_map
+from typing import Dict
+from utils import get_json
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """Test case for the `access_nested_map` function."""
-
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-    def test_access_nested_map(self, nested_map: Mapping[str, Any], path: Sequence[str], expected: Any) -> None:
-        """
-        Test that `access_nested_map` returns the correct value for given inputs.
-
-        Parameters:
-        - nested_map: a nested dictionary to be accessed.
-        - path: a sequence of keys representing the path to the value.
-        - expected: the expected value at the given path.
-        """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+class TestGetJson(unittest.TestCase):
+    """Test case for the `get_json` function."""
 
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
     ])
-    def test_access_nested_map_exception(self, nested_map: Mapping[str, Any], path: Sequence[str]) -> None:
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
         """
-        Test that `access_nested_map` raises a KeyError for invalid paths.
+        Test that `get_json` returns the expected result.
 
         Parameters:
-        - nested_map: a nested dictionary to be accessed.
-        - path: a sequence of keys representing the path to the value.
+        - test_url: the URL to fetch the JSON data from.
+        - test_payload: the expected JSON payload.
         """
-        with self.assertRaises(KeyError) as cm:
-            access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), str(path[-1]))
+        with patch('requests.get') as mocked_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mocked_get.return_value = mock_response
+
+            result = get_json(test_url)
+            mocked_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
